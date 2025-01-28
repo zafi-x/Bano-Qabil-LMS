6fc85f700aea96abdedaf56c5caa2642c23d7d12
zafi-x <huzaifabhai915@gmail.com>
2025-01-28 13:41:38 +0500

    Applied .gitignore to remove unnecessary files

--- lib/ui/onboarding/onboarding_Screen.dart
+++ lib/ui/onboarding/onboarding_Screen.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
 
-class OnboardingScreen extends StatelessWidget {
-  const OnboardingScreen({super.key});
-
+class OnBoarding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: OnBoardingSlider(
+        headerBackgroundColor: Colors.white,
+        finishButtonText: 'Register',
+        finishButtonStyle: const FinishButtonStyle(
+          backgroundColor: Colors.black,
+        ),
+        skipTextButton: const Text('Skip'),
+        trailing: const Text('Login'),
+        background: [
+          Image.asset('assets/onboarding.png'),
+          Image.asset('assets/onboarding3.png'),
+        ],
+        totalPage: 3,
+        speed: 1.8,
+        pageBodies: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: const Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 480,
+                ),
+                Text('Description Text 1'),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: const Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 480,
+                ),
+                Text('Description Text 2'),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: const Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 480,
+                ),
+                Text('Description Text 3'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/ui/splash/splah.dart
+++ lib/ui/splash/splah.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:qabilacademy/ui/onboarding/onboarding_Screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -17,10 +18,10 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(
       const Duration(seconds: 1),
       () {
-        // Navigator.pushReplacement(
-        //   context,
-        //   MaterialPageRoute(builder: (context) => const HomePageScreen()),
-        // );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => OnBoarding()),
+        );
       },
     );
   }
@@ -30,13 +31,36 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: SizedBox(
-          height: 210.h,
-          width: 250.w,
-          child: Image.asset(
-            "assets/logo.png",
-            fit: BoxFit.fill,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 180.h,
+              width: 260.w,
+              child: Image.asset(
+                "assets/logo.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Bno Qabil LMS",
+                style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                      color: const Color.fromARGB(
+                        255,
+                        45,
+                        142,
+                        120,
+                      ),
+                      letterSpacing: .5,
+                      fontSize: 28.sp,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
